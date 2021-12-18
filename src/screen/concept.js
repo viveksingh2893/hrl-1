b8088ca58b42723a7280d2e342852162c3e7b261
shashwatpandey77 <your_github_email>
2021-12-18 21:29:08 +0530

    changes by shashwat

--- src/screen/concept.js
+++ src/screen/concept.js
@@ -1,31 +1,65 @@
 import { useLocation } from "react-router-dom";
-import {Image, Layout,Typography} from 'antd'
-import conceptimg from '../assets/image/img2.webp';
-import '../App.less'
+import { Image, Layout, Typography, Space, Card } from "antd";
+import conceptimg from "../assets/image/IMG 2.1.png";
+import "../App.less";
+import Descard from "../components/descard";
 
-const Concept=()=>{
-    const location=useLocation()
-    const {Content}=Layout
-    const {Title,Text}=Typography
+const Concept = () => {
+  const location = useLocation();
+  const { Content } = Layout;
+  const { Title, Text } = Typography;
+  const what_data = `WHAT- ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+  nisi ut aliquip ex ea commodo consequat.`;
 
-    return (
-       <Layout>
-           <Content>
-               <Image
-               
-
-               src={conceptimg}/>  
-               
-           </Content>
-           <Title class="title">
-           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-           </Title>
-           <Title level={3}>
-           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-           </Title>
-           
-       </Layout>
-    )
-}
+  return (
+    <Layout>
+      <Space direction="vertical">
+        {/* <div
+          style={{
+            display: "flex",
+            width: "100vw",
+            height: "40vw",
+            // background: `url("${conceptimg}")`,
+            // backgroundSize: "cover",
+            justifyContent: "center",
+            // backgroundSize: "cover",
+            backgroundColor: "white",
+          }}
+        >
+          <Image
+            src={conceptimg}
+            style={{ width: "100vw", height: "40vw", objectFit: "cover" }}
+          />
+        </div> */}
+        <Descard txt={what_data}></Descard>
+        <div
+          style={{
+            display: "flex",
+            width: "100vw",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            marginTop: "1vw",
+          }}
+        >
+          <div
+            style={{
+              width: "80vw",
+            }}
+            class="video-container"
+          >
+            <iframe
+              src="https://www.youtube.com/embed/ctJFwgh4yds"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      </Space>
+    </Layout>
+  );
+};
 
 export default Concept;
--- src/screen/home.js
+++ src/screen/home.js
@@ -1,32 +1,82 @@
-import {Menu,Layout,Button,Image, Typography} from 'antd'
-import img2 from '../assets/image/img2.webp';
-import ConceptW from '../components/conceptwhite';
-import ConceptB from '../components/conceptblue';
-const Home=()=>{
-    const {Sider,Content,Header,Footer} = Layout
-    const {Title} = Typography
-    return(
-        <Content style={{alignItems:'center', width:'100vw',justifyContent:'center', display:'flex',flexDirection:'column'}}>
-        
-<Image
-width='100vw'
-preview={false}
-src={img2}
-/>
-<Title level={4} style={{flexWrap:'wrap',fontWeight:'normal',width:'50vw',textAlign:'center',padding:'1.5vw' ,fontSize:'2.5vw'}}>
-"Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-</Title >
- 
-   <ConceptB/>
-   <ConceptW/>
-   <ConceptB/>
-   <ConceptW/>
-   <ConceptB/>
-   <ConceptW/>
+import { Menu, Layout, Button, Image, Typography } from "antd";
+import img2 from "../assets/image/IMG 0.2A.jpeg";
+import ConceptW from "../components/conceptwhite";
+import ConceptB from "../components/conceptblue";
+const Home = () => {
+  const { Sider, Content, Header, Footer } = Layout;
+  const { Title } = Typography;
+  return (
+    <Content
+      style={{
+        alignItems: "center",
+        width: "100vw",
+        justifyContent: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {/* <div
+        style={{
+          display: "flex",
+          width: "100vw",
+          height: "30vw",
+          justifyContent: "flex-start",
+          alignItems: "center",
+          flexDirection: "row",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            borderBottom: "30vw solid #ffcc00",
+            borderLeft: "0px solid transparent",
+            borderRight: "10vw solid transparent",
+            height: "0",
+            width: "50vw",
+          }}
+        ></div>
+        <div
+          style={{
+            borderTop: "30vw solid #000",
+            borderRight: "0vw solid transparent",
+            borderLeft: "10vw solid transparent",
+            height: "0",
+            width: "100vw",
+            justifyContent: "flex-end",
+            alignItems: "flex-end",
+            display: "flex",
+          }}
+        >
+          <Image
+            preview={false}
+            src={img2}
+            style={{ objectFit: "contain", width: "45vw" }}
+          />
+        </div>
+      </div> */}
+      <Image width="100vw" height="30vw" preview={true} src={img2} />
+      <Title
+        level={4}
+        style={{
+          flexWrap: "wrap",
+          fontWeight: "normal",
+          width: "50vw",
+          textAlign: "center",
+          padding: "1.5vw",
+          fontSize: "2.5vw",
+        }}
+      >
+        "Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. "
+      </Title>
 
-  
-
-</Content>
-    )
-}
+      <ConceptB />
+      <ConceptW />
+      <ConceptB />
+      <ConceptW />
+      <ConceptB />
+      <ConceptW />
+    </Content>
+  );
+};
 export default Home;
