0c012b25e3132a8cbaca6df5168714789d0bf0e2
RaCoDev <itsmerana7@gmail.com>
2021-12-20 09:15:17 +0530

    header seperated

--- src/App.js
+++ src/App.js
@@ -1,89 +1,28 @@
-import logo from "./logo.svg";
-import { Menu, Layout, Button, Image, Typography } from "antd";
-import img2 from "./assets/image/img2.webp";
+
+import { Layout } from "antd";
 import "./App.less";
 
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
-  Link,
-  useNavigate,
+  
 } from "react-router-dom";
-import ConceptW from "./components/conceptwhite";
 import Gallery from "./screen/gallery";
 import Blog from "./screen/blog";
 import Home from "./screen/home";
 import Concept from "./screen/concept";
-// import Home from './screen/home';
+import Headbar from "./components/header";
+
 
 function App() {
-  const { SubMenu } = Menu;
-  const navigate = useNavigate();
-  const { Sider, Content, Header, Footer } = Layout;
-  const { Text, Title } = Typography;
-  const data = [
-    {
-      title: "ABOUT",
-      link: ["details", "team", "admin", "contact"],
-      item: ["HRL DETAILS", "TEAM", "ADMIN", "CONTACT"],
-    },
-    {
-      title: "CONCEPT",
-      link: ["concept", "concept", "concept"],
-      item: ["MITH.AI", "DHARA", "SKY BRIDGE"],
-    },
-    {
-      title: "RESOURCE",
-      link: ["blog", "gallery", "news"],
-      item: ["BLOG", "GALLERY", "NEWS"],
-    },
-    { title: "LOGIN/REGISTER", link: [], item: [] },
-  ];
+  const { Footer } = Layout;
+
 
   return (
-    <Layout>
-      <Header
-        style={{
-          width:'100vw',
-          justifyContent: "space-around",
-          alignItems: "center",
-          backgroundColor: "#ffcc00",
-        }}
-      >
-        {/* <div className='logo'>Hello</div> */}
-        {/* <div style={{float:'left'}}>
-          <h1>hi rapid lab</h1>
-        </div> */}
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          style={{
-            justifyContent: "flex-end",
-            backgroundColor: "#ffcc00",
-          }}
-        >
-          {data.map((val, ind) => {
-            return (
-              <SubMenu key={`${ind}`} title={val.title}>
-                {val.item.map((newVal, index) => {
-                  return (
-                    <Menu.Item
-                      onClick={() =>
-                        navigate(`/${val.link[index]}`, { state: newVal })
-                      }
-                    >
-                      {" "}
-                      {newVal}
-                    </Menu.Item>
-                  );
-                })}
-              </SubMenu>
-            );
-          })}
-        </Menu>
-      </Header>
-
+    <div>
+      <Headbar/>
+      
+          <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog page="blog" />} />
@@ -93,7 +32,8 @@ function App() {
       <Footer style={{ textAlign: "center" }}>
         Ant Design ©2018 Created by Ant UED
       </Footer>
-    </Layout>
+      </Layout>
+    </div>
   );
 }
 
