4ac8a06a24974f667be5efbead11369925b6f43d
RaCoDev <itsmerana7@gmail.com>
2021-12-21 12:51:22 +0530

    margin added

--- src/screen/home.js
+++ src/screen/home.js
@@ -9,69 +9,39 @@ const Home = () => {
   return (
     <Content
       style={{
-        width:'100vw',
+        
         alignItems: "center",
         justifyContent: "center",
-        marginTop:'10vh',
+        marginTop:'calc(20px + 2.5vw )',
+        width:"100vw",
 
         display: "flex",
         flexDirection: "column",
       }}
     >
-      {/* <div
-        style={{
-          display: "flex",
-          width: "100vw",
-          height: "30vw",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          flexDirection: "row",
-        }}
-      >
-        <div
-          style={{
-            position: "absolute",
-            borderBottom: "30vw solid #ffcc00",
-            borderLeft: "0px solid transparent",
-            borderRight: "10vw solid transparent",
-            height: "0",
-            width: "50vw",
-          }}
-        ></div>
-        <div
-          style={{
-            borderTop: "30vw solid #000",
-            borderRight: "0vw solid transparent",
-            borderLeft: "10vw solid transparent",
-            height: "0",
-            width: "100vw",
-            justifyContent: "flex-end",
-            alignItems: "flex-end",
-            display: "flex",
-          }}
-        >
-          <Image
-            preview={false}
-            src={img2}
-            style={{ objectFit: "contain", width: "45vw" }}
-          />
-        </div>
-      </div> */}
-      <Image width="100vw"  preview={true} src={img2} />
+     
+      <Image 
+       width="100vw" 
+       style={{border:'2px solid black'}} 
+       preview={false} src={img2} />
+
+      
+  
       <Title
         level={4}
         style={{
-          flexWrap: "wrap",
           fontWeight: "normal",
-          width: "50vw",
           textAlign: "center",
-          padding: "1.5vw",
-          fontSize: "2.5vw",
+          
+          fontSize: "2.5vh",
         }}
       >
         "Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. "
       </Title>
+
+    
+     
       <ConceptW />
       <ConceptB />
       <ConceptW />
