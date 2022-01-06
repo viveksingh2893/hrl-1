0eeac0f7a6242055155ad1e474b7c26275a46feb
shashwatpandey77 <your_github_email>
2022-01-06 16:27:56 +0530

    gallery search enabled

--- src/screen/gallery.js
+++ src/screen/gallery.js
@@ -4,18 +4,46 @@ import { useEffect, useState } from "react";
 import Wordcloud from "../components/wordcloud";
 import Searchbar from "../components/searchbar";
 import Mapbox from "../components/Mapbox";
-import { Divider, List } from "antd";
+import { Typography, Divider, List, Button } from "antd";
 import { useNavigate } from "react-router-dom";
 import ProfileCard from "../components/profilecard";
 
 const Gallery = () => {
-  const onSearch = (value) => console.log(value);
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const [viewPortWidth, setWidth] = useState(0);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", (e) => {
+      console.log("size", e.target);
+      setWidth(window.innerWidth);
+    });
+  }, []);
+  const [srchres, setSrchres] = useState("none");
+  const onSearch = (value) => {
+    setSrchres(value);
+    setSearchShow(true);
+  };
+  const clearSearch = () => {
+    setSearchShow(false);
+  };
+  const { Title, Text } = Typography;
+  const [searchshow, setSearchShow] = useState(false);
   const navigate = useNavigate();
   const data = [];
   const keywords = [];
+  const searchdata = [];
+  function txtlvl() {
+    if (viewPortWidth > 600) {
+      return 3;
+    } else {
+      return 5;
+    }
+  }
+
+  let tlvl = txtlvl();
+
   for (let i = 0; i < 21; i++) {
     data.push({
       href: "https://ant.design",
@@ -30,14 +58,52 @@ const Gallery = () => {
       id: i,
       keyword: `word ${String(i)}`,
     });
+    searchdata.push({
+      href: "https://ant.design",
+      name: `ant design part ${i}`,
+      avatar: `https://picsum.photos/id/${i + 100}/300/200`,
+      description:
+        "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+      content:
+        "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+    });
   }
 
   return (
     <div className="container-layout">
       <Wordcloud data={keywords}></Wordcloud>
-      <Searchbar width="40vw" onSearch={onSearch}></Searchbar>
-      <Divider></Divider>
+
       <Mapbox></Mapbox>
+      <Divider></Divider>
+      <Searchbar width="40vw" onSearch={onSearch}></Searchbar>
+      {searchshow ? (
+        <div
+          style={{
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "5vw",
+            flexDirection: "column",
+          }}
+        >
+          <Typography.Title
+            // style={{ marginLeft: "10vw" }}
+            level={tlvl}
+            style={{ fontFamily: "calibri" }}
+          >{`${searchdata.length} SEARCH RESULTS FOR "${srchres}"`}</Typography.Title>
+          <Button
+            type="primary"
+            shape="round"
+            size="small"
+            onClick={clearSearch}
+          >
+            Clear Results
+          </Button>
+        </div>
+      ) : (
+        <div></div>
+      )}
       <List
         bordered
         style={{ padding: 100, borderWidth: 0 }}
@@ -50,7 +116,7 @@ const Gallery = () => {
           },
           pageSize: 5,
         }}
-        dataSource={data}
+        dataSource={searchshow == false ? data : searchdata}
         renderItem={(item) => (
           <List.Item>
             <ProfileCard
