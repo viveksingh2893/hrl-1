f1011bfdf7b2128bde11bf191310fc335ff84dd4
shashwatpandey77 <your_github_email>
2022-01-06 14:05:06 +0530

    Search feature enabled

--- src/screen/blog.js
+++ src/screen/blog.js
@@ -3,16 +3,43 @@ import "../App.less";
 import Wordcloud from "../components/wordcloud";
 import Searchbar from "../components/searchbar";
 import { useEffect, useState } from "react";
-import { Typography, Divider, Image } from "antd";
+import { Typography, Divider, Image, Button } from "antd";
 import { List } from "antd";
 import img1 from "../assets/image/IMG 2.4C.jpg";
 import ProfileCard from "../components/profilecard";
 
 const Blog = () => {
-  const onSearch = (value) => console.log(value);
-
+  const [viewPortWidth, setWidth] = useState(0);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", (e) => {
+      console.log("size", e.target);
+      setWidth(window.innerWidth);
+    });
+  }, []);
+  const [srchres, setSrchres] = useState("none");
+  const onSearch = (value) => {
+    setSrchres(value);
+    setSearchShow(true);
+  };
+  const clearSearch = () => {
+    setSearchShow(false);
+  };
+  const { Title, Text } = Typography;
+  const [searchshow, setSearchShow] = useState(false);
   const data = [];
   const keywords = [];
+  const searchdata = [];
+  function txtlvl() {
+    if (viewPortWidth > 600) {
+      return 3;
+    } else {
+      return 5;
+    }
+  }
+
+  let tlvl = txtlvl();
+
   for (let i = 0; i < 21; i++) {
     data.push({
       href: "https://ant.design",
@@ -27,7 +54,17 @@ const Blog = () => {
       id: i,
       keyword: `word ${String(i)}`,
     });
+    searchdata.push({
+      href: "https://ant.design",
+      name: `ant design part ${i}`,
+      avatar: `https://picsum.photos/id/${i + 100}/300/200`,
+      description:
+        "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+      content:
+        "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+    });
   }
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -40,6 +77,34 @@ const Blog = () => {
       <div>
         <Searchbar width="40vw" onSearch={onSearch}></Searchbar>
       </div>
+      {searchshow ? (
+        <div
+          style={{
+            width: "100vw",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "5vw",
+            flexDirection: "column",
+          }}
+        >
+          <Typography.Title
+            // style={{ marginLeft: "10vw" }}
+            level={tlvl}
+            style={{ fontFamily: "calibri" }}
+          >{`${searchdata.length} SEARCH RESULTS FOR "${srchres}"`}</Typography.Title>
+          <Button
+            type="primary"
+            shape="round"
+            size="small"
+            onClick={clearSearch}
+          >
+            Clear Results
+          </Button>
+        </div>
+      ) : (
+        <div></div>
+      )}
       <Divider></Divider>
       <div style={{ width: "80vw" }}>
         <List
@@ -63,7 +128,7 @@ const Blog = () => {
             },
             pageSize: 4,
           }}
-          dataSource={data}
+          dataSource={searchshow == false ? data : searchdata}
           renderItem={(item) => (
             <List.Item>
               <div
@@ -74,11 +139,7 @@ const Blog = () => {
                   flexWrap: "wrap",
                 }}
               >
-                <Image
-                  width={272}
-                  alt="logo"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                />
+                <Image width={272} alt="logo" src={item.avatar} />
                 <Divider type="vertical"></Divider>
                 <List.Item.Meta
                   style={{ display: "flex", flexBasis: 400 }}
