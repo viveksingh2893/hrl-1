46e905ab8ab059dd4a907813679549a3ca17d279
shashwatpandey77 <your_github_email>
2022-02-06 00:33:34 +0530

    Blog&News completed with clickable wordcloud

--- src/components/previewmodal.js
+++ src/components/previewmodal.js
@@ -1,119 +1,151 @@
-import { Card,Typography,Button,Image,Divider,Row} from 'antd';
-import { CloudUploadOutlined } from '@ant-design/icons';
-import React, { useEffect,useState } from 'react';
+import { Card, Typography, Button, Image, Divider, Row } from "antd";
+import { CloudUploadOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const PreviewModal=(props)=>{
-    const {Title,Text,Paragraph}=Typography;
-        let TDate=new Date()
-        // let date = new Date( Date.parse(props.date) );
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const PreviewModal = (props) => {
+  const { Title, Text, Paragraph } = Typography;
+  let TDate = new Date();
+  // let date = new Date( Date.parse(props.date) );
 
-// console.log('aa',TDate,date)
-    
-const {title,keywords}=props.data()
-const keylist=keywords.split(',')
-console.log(TDate)
-    const textData=(content)=>{
-        return(
-           
-            <Paragraph 
-            style={{textAlign:'justify',fontSize:'1.2rem',color:'black'}} 
-            copyable={false}>{content}
-            </Paragraph>
-           
-        )
-    }
-    const imgData=({image,caption})=>{
-        return(
-            <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center',
-            marginTop:'1vw', marginBottom:'1vw'}}>
-            <Image src={image} width='60vw'/>
-            <Text style={{textAlign:'start',width:'60vw',fontWeight:'400'}}>{caption}</Text>
-            </div>
-        )
-    }
-    const vidData=(content)=>{
-        return(
-            <div style={{ position: "relative",
-                width: '100%',
-                overFlow: "hidden",
-                paddingTop: '56.25%',
-                marginTop:'1vw', marginBottom:'1vw'}}>
-            <iframe src={content} style={{
-                 position: 'absolute',
-                 top: 0,
-                 left: 0,
-                 bottom: 0,
-                 right: 0,
-                 width: "100%",
-                 height: "100%",
-                 border: "none",
-            }}/>
-            </div>
-        )
-    }
-return(
-    <div style={{display:'flex',flexDirection:'column',justifyContent:'center' ,alignItems:'center',width:'60vw'}}>
-        <div style={{display:'flex',flexDirection:'column',width:'60vw'}}>
-            <Title style={{textAlign:'justify'}}>{title}</Title>
-            <Text style={{textAlign:'justify',fontWeight:'700',fontSize:'14px'}}>By {props.author}</Text>
-            <Text style={{textAlign:'justify',fontWeight:'600',fontSize:'12px'}}>
-                {props.date?`${months[props.date.getMonth()]} ${props.date.getDate()} ${props.date.getFullYear()}`:`${months[TDate.getMonth()]} ${TDate.getDate()} ${TDate.getFullYear()}`}
-                </Text>
-            <Divider></Divider>
-            {props.body.map((val,index)=>{
-                if(val.type=='text'){
-                    console.log(val,'values')
-                    return(
-                        <div>
-                            {textData(val.content)}
-                        </div>
-                     )
-                }else if(val.type=='image'){
-                    return(
-                        <div>
-                            {imgData({image:val.image,caption:val.description})}
-                        </div>
-                     )
-                }else if(val.type=='video'){
-                    return(
-                        <div>
-                            {vidData(val.video)}
-                        </div>
-                     )
-                }
-                
-            })}
-           <Divider>
-           </Divider>
-            <Row>
-             { keylist.map((value,index)=>{
+  // console.log('aa',TDate,date)
 
-                return(
-                    <Button  size='middle'
-                    style={{
-                    
-                    fontWeight:'600',
-                    border:'2px solid #666666',
-                    margin:'0.2vw'
-                    }}>
-                    {value.toUpperCase()}
-                    </Button>
-                   
-                
-
-                )
-                })
-                }       
-                 
-                 
-                 
-        </Row>   
-            
-
-        </div>
+  const { title, keywords } = props.data();
+  const keylist = keywords.split(",");
+  console.log(TDate);
+  const textData = (content) => {
+    return (
+      <Paragraph
+        style={{ textAlign: "justify", fontSize: "1.2rem", color: "black" }}
+        copyable={false}
+      >
+        {content}
+      </Paragraph>
+    );
+  };
+  const imgData = ({ image, caption }) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "1vw",
+          marginBottom: "1vw",
+        }}
+      >
+        <Image src={image} width="60vw" preview={false} />
+        <Text style={{ textAlign: "start", width: "60vw", fontWeight: "400" }}>
+          {caption}
+        </Text>
+      </div>
+    );
+  };
+  const vidData = (content) => {
+    return (
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          overFlow: "hidden",
+          paddingTop: "56.25%",
+          marginTop: "1vw",
+          marginBottom: "1vw",
+        }}
+      >
+        <iframe
+          src={content}
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            width: "100%",
+            height: "100%",
+            border: "none",
+          }}
+        />
+      </div>
+    );
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "60vw",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column", width: "60vw" }}>
+        <Title style={{ textAlign: "justify" }}>{title}</Title>
+        <Text
+          style={{ textAlign: "justify", fontWeight: "700", fontSize: "14px" }}
+        >
+          By {props.author}
+        </Text>
+        <Text
+          style={{ textAlign: "justify", fontWeight: "600", fontSize: "12px" }}
+        >
+          {props.date
+            ? `${
+                months[props.date.getMonth()]
+              } ${props.date.getDate()} ${props.date.getFullYear()}`
+            : `${
+                months[TDate.getMonth()]
+              } ${TDate.getDate()} ${TDate.getFullYear()}`}
+        </Text>
+        <Divider></Divider>
+        {props.body.map((val, index) => {
+          if (val.type == "text") {
+            console.log(val, "values");
+            return <div>{textData(val.content)}</div>;
+          } else if (val.type == "image") {
+            return (
+              <div>
+                {imgData({ image: val.image, caption: val.description })}
+              </div>
+            );
+          } else if (val.type == "video") {
+            return <div>{vidData(val.video)}</div>;
+          }
+        })}
+        <Divider></Divider>
+        <Row>
+          {keylist.map((value, index) => {
+            return (
+              <Button
+                size="middle"
+                style={{
+                  fontWeight: "600",
+                  border: "2px solid #666666",
+                  margin: "0.2vw",
+                }}
+              >
+                {value.toUpperCase()}
+              </Button>
+            );
+          })}
+        </Row>
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default PreviewModal;
