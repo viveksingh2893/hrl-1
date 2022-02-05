5fa6eea03fa559830971552fce377a0c33d998b3
shashwatpandey77 <your_github_email>
2022-02-05 22:17:24 +0530

    Home Page changes complete

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,107 +1,76 @@
 import { Menu } from "antd";
-import {useNavigate, useParams,NavLink} from "react-router-dom";
-const NavMenu=(props)=>{
-    const { SubMenu } = Menu;
-    const navigate = useNavigate();
-    console.log("....................",window.scroll)
-    
-    const DEFAULTSTYLE={
-      fontSize:'calc(10px + 0.5vw)',
-      fontWeight:'bolder',
-      height:'80px',
-      
-      fontFamily:'Calibri',
-      justifyContent: "flex-end",
-      backgroundColor: "#666666",
-    }
-    const navstyle= props.style?props.style:DEFAULTSTYLE
-    return (
+import { useNavigate, useParams, NavLink } from "react-router-dom";
+const NavMenu = (props) => {
+  const { SubMenu } = Menu;
+  const navigate = useNavigate();
+  console.log("....................", window.scroll);
 
-        <Menu
-        theme="dark"
-          
-          mode= {props.viewPortWidth>700?"horizontal":"inline"}
-          style={navstyle}
-        >
-         
-              <SubMenu  key='about' title='About'>
-              
-                    <Menu.Item key='detail'
-                      onClick={()=>navigate('/detail')
-                    }
-                    >
-                     Hrl details
-                    </Menu.Item>
-                    <Menu.Item key='team'
-                      onClick={() =>
-                        navigate(`/team`)
-                      }
-                    >
-                     Team
-                    </Menu.Item>
-                    <Menu.Item key='contact'
-                   onClick={()=>navigate('/contact')
-                  }
-                    
-                    >
-                    Contact
-                    </Menu.Item>
-                  
-               
-                
-              </SubMenu>
-              <SubMenu  key='concept' title='Concept'>
-                {props.concept?props.concept.map((value,index)=>{
-                  return (
-                   
-                    <Menu.Item 
-                    key={`${value.conceptname}`}
-                   
-                // onClick={() =>
-                //   navigate(`/concept/${value.conceptname}`,{state:value.conceptname})
-                // }
-              > <NavLink to={`/concept/${value.conceptname}`}>
-                {value.conceptname}</NavLink>
-              </Menu.Item>
-              
-                    
+  const DEFAULTSTYLE = {
+    fontSize: "calc(10px + 0.5vw)",
+    fontWeight: "bolder",
+    height: "80px",
 
-                  )
-                })
-               
-                :null}
-                </SubMenu>
-              <SubMenu key='resource' title='Resource'>
-              <Menu.Item key='blog'
-              >
-               <NavLink to='/resource/blog'>Blog</NavLink> 
-              </Menu.Item>
-              <Menu.Item key='news'>
-               <NavLink to='/resource/news'>News</NavLink> 
-              </Menu.Item>
-              <Menu.Item key='gallery'
-                onClick={() =>
-                  navigate(`/gallery`)
-                }
-              >
-               Gallery
-              </Menu.Item>
-              </SubMenu>
-            
-         
-          
-       
-                
-         
-        
-          <SubMenu key={`login`} title='Login'
-                onTitleClick={() =>
-                  navigate(`/login`)
-                }
-              ></SubMenu>
-        </Menu>
+    fontFamily: "Calibri",
+    justifyContent: "flex-end",
+    backgroundColor: "#666666",
+  };
+  const navstyle = props.style ? props.style : DEFAULTSTYLE;
+  return (
+    <Menu
+      theme="dark"
+      mode={props.viewPortWidth > 700 ? "horizontal" : "inline"}
+      style={navstyle}
+    >
+      <SubMenu key="about" title="About">
+        <Menu.Item key="detail" onClick={() => navigate("/detail")}>
+          HRL Details
+        </Menu.Item>
+        <Menu.Item key="team" onClick={() => navigate(`/team`)}>
+          Team
+        </Menu.Item>
+        <Menu.Item key="contact" onClick={() => navigate("/contact")}>
+          Contact
+        </Menu.Item>
+      </SubMenu>
+      <SubMenu key="concept" title="Concept">
+        {props.concept
+          ? props.concept.map((value, index) => {
+              return (
+                <Menu.Item
+                  key={`${value.conceptname}`}
 
-    )
-}
+                  // onClick={() =>
+                  //   navigate(`/concept/${value.conceptname}`,{state:value.conceptname})
+                  // }
+                >
+                  {" "}
+                  <NavLink to={`/concept/${value.conceptname}`}>
+                    {value.conceptname}
+                  </NavLink>
+                </Menu.Item>
+              );
+            })
+          : null}
+      </SubMenu>
+      <SubMenu key="resource" title="Resource">
+        <Menu.Item key="blog">
+          <NavLink to="/resource/blog">Blog</NavLink>
+        </Menu.Item>
+        <Menu.Item key="news">
+          <NavLink to="/resource/news">News</NavLink>
+        </Menu.Item>
+        <Menu.Item key="gallery" onClick={() => navigate(`/gallery`)}>
+          Gallery
+        </Menu.Item>
+      </SubMenu>
+
+      <SubMenu
+        key={`login`}
+        title="Login"
+        onTitleClick={() => navigate(`/login`)}
+      ></SubMenu>
+    </Menu>
+  );
+};
 
 export default NavMenu;
--- src/screen/contact.js
+++ src/screen/contact.js
@@ -1,50 +1,45 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 import ConceptW from "../components/conceptwhite";
 
-
 const Contact = () => {
-    const [width,setWidth]=useState(0)
-    useEffect(()=>{
-      window.scrollTo(0,0)
-    },[])
-    useEffect(()=>{
-        setWidth(window.innerWidth)
-        window.addEventListener('resize',(e)=>{
-         setWidth(window.innerWidth)
-        })
-      },[])
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+    window.addEventListener("resize", (e) => {
+      setWidth(window.innerWidth);
+    });
+  }, []);
 
   return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 100,
+      }}
+    >
       <div
         style={{
-          display:"flex",
-         width:'100vw',
-          alignItems:"center",
-          justifyContent:"center",
-          marginTop:100,
-         
+          display: "flex",
+          width: "80vw",
+          height: "80vh",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-       
-<div style={{
-          display:"flex",
-         width:'80vw',
-         height:'80vh',
-          alignItems:"center",
-          justifyContent:"center",
-         
-        }}>
-        
-        <ConceptW width={width} title='Contact Us'
+        {/* <ConceptW width={width} title='Contact Us'
         flexsize={1}
         btitle='Discover More'
-        /></div>
-       
-       
+        /> */}
       </div>
+    </div>
   );
-}
-
+};
 
 export default Contact;
